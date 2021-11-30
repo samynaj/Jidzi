45e7a5f5b2cd0b3812f55548e59d568c9cadb429
samynaj <samuelnnj@gmail.com>
2021-11-30 16:16:42 +0100

    first commit

---
+++ src/Pages/onboarding/WelcomeScreen/WelcomeScreen.jsx
@@ -0,0 +1,68 @@
+import { Box, Typography, Button } from '@mui/material'
+import React from 'react'
+
+function WelcomeScreen() {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                width: '100%',
+                height: '100vh',
+                background: '#ffffff',
+                p:'20px',
+                overflowX: 'hidden'
+            }}
+        >
+            <Typography 
+                sx={{
+                    fontWeight: 700,
+                    fontSize: '30px',
+                    lineHeight: '42.3px',
+                    color: '#001533'
+                }}
+            >
+                Welcome to Jidzi
+            </Typography>
+            <Typography
+                sx={{
+                    fontWeight: 400,
+                    fontSize: '17px',
+                    lineHeight: '25.5px',
+                    color: '#001533',
+                    mb: '172px'
+                }}
+            >
+                #1 business growth partner
+            </Typography>
+            <Button
+                sx={{
+                    color: '#ffffff',
+                    background: '#0066F6',
+                    width: '100%',
+                    height: '50px',
+                    borderRadius: '5px',
+                    mb: '20px'
+                }}
+            >
+                create your free account
+            </Button>
+            <Button
+                sx={{
+                    color: '#001533',
+                    background: '#ffffff',
+                    width: '100%',
+                    height: '50px',
+                    border: '1px solid #999999',
+                    borderRadius: '5px'
+                }}
+            >
+                log into your account
+            </Button>
+        </Box>
+    )
+}
+
+export default WelcomeScreen
