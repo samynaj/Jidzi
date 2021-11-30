5447fea54e346dc80aa4c103ba44008da61184f7
samynaj <samuelnnj@gmail.com>
2021-11-30 19:08:21 +0100

    app js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
+import ForgotPasswordScreen from './Pages/account/ForgotPasswordScreen/ForgotPasswordScreen';
+// import RecoverAcountScreen from './Pages/account/RecoverAcountScreen/RecoverAcountScreen';
+// import VerifyAccountScreen from './Pages/account/VerifyAccountScreen/VerifyAccountScreen';
 // import SigninScreen from './Pages/onboarding/SigninScreen/SigninScreen';
 // import SignupScreen from './Pages/onboarding/SignupScreen/SignupScreen';
-import SplashScreen from './Pages/onboarding/SplashScreen/SplashScreen'
+// import SplashScreen from './Pages/onboarding/SplashScreen/SplashScreen'
 // import WelcomeScreen from './Pages/onboarding/WelcomeScreen/WelcomeScreen';
 
 function App() {
   return (
     <div className="App">
-      <SplashScreen />
+      <ForgotPasswordScreen />
     </div>
   );
 }
