aaa8c2aa20b5580637cfc044e3d56b8feacca834
samynaj <samuelnnj@gmail.com>
2021-11-30 16:14:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+// import SigninScreen from './Pages/onboarding/SigninScreen/SigninScreen';
+// import SignupScreen from './Pages/onboarding/SignupScreen/SignupScreen';
+import SplashScreen from './Pages/onboarding/SplashScreen/SplashScreen'
+// import WelcomeScreen from './Pages/onboarding/WelcomeScreen/WelcomeScreen';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SplashScreen />
     </div>
   );
 }
