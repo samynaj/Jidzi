a32814f005df797e32c478ca62bb08e536296e26
samynaj <samuelnnj@gmail.com>
2021-11-30 19:09:01 +0100

    recover account

--- src/Pages/account/RecoverAcountScreen/RecoverAcountScreen.jsx
+++ src/Pages/account/RecoverAcountScreen/RecoverAcountScreen.jsx
@@ -1,10 +1,115 @@
 import React from 'react'
+import { Box, Typography, Button } from '@mui/material'
+import { MdClear } from 'react-icons/md'
 
 function RecoverAcountScreen() {
     return (
-        <div>
-            
-        </div>
+        <Box
+            sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'flex-start',
+                alignItems: 'left',
+                gap: '65px',
+                width: '100%',
+                height: '100%',
+                background: '#ffffff',
+                p:'46.42px 20px 20px',
+                overflowX: 'hidden'
+            }}
+        >
+            <MdClear fontSize='30px' />
+            <Box>
+                <Typography 
+                    sx={{
+                        fontWeight: 700,
+                        fontSize: '35px',
+                        lineHeight: '52.71px',
+                        color: '#0066F6',
+                        mb: '7px'
+                    }}
+                >
+                    Recover password
+                </Typography>
+                <Typography
+                    sx={{
+                        fontWeight: 400,
+                        fontSize: '15px',
+                        lineHeight: '22.59px',
+                        color: '#001533',
+                    }}
+                >
+                    Please enter your new password to continue
+                </Typography>
+            </Box>
+            <Box 
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '5px',
+                    mb: '180px'
+                }}
+            >
+                <label
+                    style={{ 
+                        color: '#001533', 
+                        fontSize: '15px',
+                        lineHeight: '20px'
+                    }}  
+                    htmlFor='password'
+                >
+                    New password
+                </label>
+                <input 
+                    style={{ 
+                        border: '1px solid #e4e4e4', 
+                        borderRadius: '5px',
+                        height: '50px',
+                        padding: '16px',
+                        marginBottom: '10px'
+                    }}
+                    id='password'
+                    type="password" 
+                    placeholder='********' 
+                />
+                <label
+                    style={{ 
+                        color: '#001533', 
+                        fontSize: '15px',
+                        lineHeight: '20px'
+                    }}  
+                    htmlFor='repeat'
+                >
+                    Repeat password
+                </label>
+                <input
+                    style={{ 
+                        border: '1px solid #e4e4e4', 
+                        borderRadius: '5px',
+                        height: '50px',
+                        padding: '16px',
+                        marginBottom: '10px'
+                    }} 
+                    id='repeat' 
+                    type="password" 
+                    placeholder='********' 
+                />
+            </Box>
+            <Box>
+                <Button
+                    sx={{
+                        color: '#ffffff',
+                        background: '#0066F6',
+                        width: '100%',
+                        height: '50px',
+                        borderRadius: '5px',
+                        mb: '20px'
+                    }}
+                >
+                    Change password
+                </Button>
+            </Box>
+        </Box>
     )
 }
 
