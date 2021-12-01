094fe34d95aabad04985be8fb3ee60553e2f903c
samynaj <samuelnnj@gmail.com>
2021-12-01 17:54:18 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import './App.css';
-import ForgotPasswordScreen from './Pages/account/ForgotPasswordScreen/ForgotPasswordScreen';
-// import RecoverAcountScreen from './Pages/account/RecoverAcountScreen/RecoverAcountScreen';
-// import VerifyAccountScreen from './Pages/account/VerifyAccountScreen/VerifyAccountScreen';
-// import SigninScreen from './Pages/onboarding/SigninScreen/SigninScreen';
-// import SignupScreen from './Pages/onboarding/SignupScreen/SignupScreen';
-// import SplashScreen from './Pages/onboarding/SplashScreen/SplashScreen'
-// import WelcomeScreen from './Pages/onboarding/WelcomeScreen/WelcomeScreen';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import { routes } from './Routes/index'
+import Index from './Pages';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <ForgotPasswordScreen />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Index />} />
+          {
+            routes.map((route, i) => (
+              <Route key={i} path={route.path} element={<route.component />} />
+            ))
+          }
+        </Routes>
+      </Router>
     </div>
   );
 }
