fd15c3fc445d4992a68fa31712d33f96ce23c464
samynaj <samuelnnj@gmail.com>
2021-12-09 15:59:22 +0100

    routing

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,14 @@
 import ForgotPasswordScreen from "../Pages/account/ForgotPasswordScreen/ForgotPasswordScreen";
 import RecoverAcountScreen from "../Pages/account/RecoverAcountScreen/RecoverAcountScreen";
 import VerifyAccountScreen from "../Pages/account/VerifyAccountScreen/VerifyAccountScreen";
+import NotificationScreen from "../Pages/notification/NotificationScreen";
 import OnboardingExpensesScreen from "../Pages/onboarding/Onboarding/OnboardingExpensesScreen";
 import OnboardingProfitScreen from "../Pages/onboarding/Onboarding/OnboardingProfitScreen";
 import SigninScreen from "../Pages/onboarding/SigninScreen/SigninScreen";
 import SignupScreen from "../Pages/onboarding/SignupScreen/SignupScreen";
 import WelcomeScreen from "../Pages/onboarding/WelcomeScreen/WelcomeScreen";
+import PrivacyPolicyScreen from "../Pages/privacyPolicy/PrivacyPolicyScreen";
+import TermsAndConditionsScreen from "../Pages/termsAndConditions/TermsAndConditionsScreen";
 
 
 export const routes = [ 
@@ -40,5 +43,17 @@ export const routes = [
         {
             path: '/recover-account',
             component:  RecoverAcountScreen
+        },
+        {
+            path: '/notifications',
+            component: NotificationScreen
+        },
+        {
+            path: '/privacy-policy',
+            component: PrivacyPolicyScreen
+        },
+        {
+            path: '/terms-conditions',
+            component: TermsAndConditionsScreen
         }
 ]
